81f97677014ce67f49eff06d487ba0680aef1ac8
marcmasterbet <marc.masterbet@gmail.com>
2026-07-19 18:14:31 +0200

    fix: middleware vide pour deboguer

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname.startsWith('/dashboard')) {
-    const token = req.cookies.get('sb-eb3e496a-ff58-480b-a136-0e79368e43ce-auth-token')
-    if (!token) {
-      return NextResponse.redirect(new URL('/login', req.url))
-    }
-  }
   return NextResponse.next()
 }
 
