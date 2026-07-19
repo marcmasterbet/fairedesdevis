bb3cb68ef39be9204158a8e8b5f2a99cdb82d6a8
marcmasterbet <marc.masterbet@gmail.com>
2026-07-19 18:33:39 +0200

    feat: Google Analytics + meta title description

---
+++ app/components/GoogleAnalytics.tsx
@@ -0,0 +1,15 @@
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-7TDE91QFXY" />
+      <script dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-7TDE91QFXY');
+        `
+      }} />
+    </>
+  )
+}
